8b9230149e75b8fcb831ad1af20df96be4ee7630
Utkarsh Trivedi <utkarshtrvd27@gmail.com>
2023-10-29 12:17:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Book_Recommender_System
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "Book_Recommender_System"
+AUTHOR_USER_NAME = "Utkarsh Trivedi"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="utkarshtrvd27@gamil.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
